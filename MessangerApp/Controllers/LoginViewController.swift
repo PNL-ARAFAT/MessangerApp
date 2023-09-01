89b489623e044b68763311cc6939e33bbb0cf5b6
PNL-ARAFAT <akash.arafat@playnext-lab.co.jp>
2023-09-01 14:35:53 +0900

    login and logout added

--- MessangerApp/Controllers/LoginViewController.swift
+++ MessangerApp/Controllers/LoginViewController.swift
@@ -21,7 +21,7 @@ class LoginViewController: UIViewController {
             Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
                 
                 if let e = error {
-                    print(e.localizedDescription)
+                    print(e)
                 } else {
                     //Navigate to ChatViewController
                     self.performSegue(withIdentifier: "LoginToChat", sender: self)
